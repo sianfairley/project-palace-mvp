ed3dbad97e48fc0f860f70d71fe33f3e2485c70a
Sian Fairley <sian.fairley@gmail.com>
2024-02-23 11:25:46 +0100

    Get route added to index.js, test data file added

---
+++ data/testdata.js
@@ -0,0 +1,20 @@
+module.exports = [
+  {
+    id: 1,
+    name: "Cat plushie",
+    craft: "sewing",
+    materials: ["felt", "buttons"],
+    description: "Small black and white cat toy made of felt",
+    complete: false,
+    favourite: false,
+  },
+  {
+    id: 2,
+    name: "Flying bird",
+    craft: "origami",
+    materials: ["paper"],
+    description: "Blue paper origami bird",
+    complete: false,
+    favourite: false,
+  },
+];
--- routes/index.js
+++ routes/index.js
@@ -1,9 +1,16 @@
 var express = require("express");
 var router = express.Router();
+let data = require("../data/testdata.js");
 
 /* GET home page. */
 router.get("/", function (req, res, next) {
   res.send({ title: "Express" });
+});
+
+// GET all data
+router.get("/testdata", (req, res) => {
+  // Return all data
+  res.send(data);
 });
 
 module.exports = router;
