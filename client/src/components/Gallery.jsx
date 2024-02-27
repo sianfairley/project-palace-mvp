4ec02d3451d4ad4e31872d762579e6ad32dbd189
Sian Fairley <sian.fairley@gmail.com>
2024-02-27 09:52:14 +0100

    Bootstrap installed, delete button using model added to Gallery (currently inactive)

--- client/src/components/Gallery.jsx
+++ client/src/components/Gallery.jsx
@@ -2,6 +2,7 @@ import { MdFavorite } from "react-icons/md";
 import { MdFavoriteBorder } from "react-icons/md";
 import { MdEdit } from "react-icons/md";
 import { TiDelete } from "react-icons/ti";
+import "bootstrap/dist/css/bootstrap.css";
 
 //Split project into separate component. Pass projects as prop or use component composition?
 function Gallery({ projects }) {
@@ -38,14 +39,69 @@ function ProjectCard({ project }) {
         <button>
           {project.favourite ? <MdFavorite /> : <MdFavoriteBorder />}
         </button>
-        <button>
-          <TiDelete />
-        </button>
+        <ButtonTriggerDeleteConfirmation />
         <button>
           <MdEdit />
         </button>
       </div>
     </>
+  );
+}
+
+function ButtonTriggerDeleteConfirmation() {
+  return (
+    <button
+      type="button"
+      className="btn btn-primary"
+      data-toggle="modal"
+      data-target="#deletemodal"
+    >
+      <TiDelete />
+    </button>
+  );
+}
+
+function DeleteConfirmationModal() {
+  return (
+    <div
+      className="modal fade"
+      id="deletemodal"
+      tabindex="-1"
+      role="dialog"
+      aria-labelledby="exampleModalLabel"
+      aria-hidden="true"
+    >
+      <div className="modal-dialog" role="document">
+        <div className="modal-content">
+          <div className="modal-header">
+            <h5 className="modal-title" id="exampleModalLabel">
+              Delete this project?
+            </h5>
+            <button
+              type="button"
+              className="close"
+              data-dismiss="modal"
+              aria-label="Close"
+            >
+              <span aria-hidden="true">&times;</span>
+            </button>
+          </div>
+          <div className="modal-body">...</div>
+          <div className="modal-footer">
+            <button
+              type="button"
+              className="btn btn-secondary"
+              data-dismiss="modal"
+            >
+              Close
+            </button>
+            <button type="button" className="btn btn-primary">
+              Save changes
+            </button>
+          </div>
+        </div>
+      </div>
+    </div>
   );
 }
 
---
+++ client/src/components/create.jsx
@@ -0,0 +1,142 @@
+import { useState } from "react";
+// import Button from "./src/App.jsx";
+
+function Create({ setProjects }) {
+  const [input, setInput] = useState({
+    projectname: "",
+    type: "",
+    materials: "",
+    description: "",
+    image: "",
+    complete: false,
+    favorite: false,
+  });
+
+  //Gets input
+  const handleChange = (e) => {
+    setInput((input) => ({ ...input, [e.target.name]: e.target.value }));
+  };
+
+  const handleSubmitProject = (event) => {
+    event.preventDefault();
+    addProject();
+  };
+
+  // POST add new project
+  const addProject = () => {
+    let options = {
+      method: "POST",
+      headers: {
+        "Content-Type": "application/json",
+      },
+      body: JSON.stringify(input),
+    };
+
+    //
+    fetch("/api/projects", options)
+      .then((response) => response.json())
+      .then((data) => {
+        console.log(data);
+        setProjects(data);
+      })
+      .catch((error) => console.log(error));
+  };
+
+  return (
+    <>
+      <form onSubmit={handleSubmitProject}>
+        <h2>Create a new project</h2>
+        <div>
+          <h3>1. Give your project a name</h3>
+          <input
+            type="text"
+            label="project name"
+            value={input.projectname}
+            name="projectname"
+            onChange={handleChange}
+          ></input>
+        </div>
+        <div>
+          <h3>2. Choose a craft</h3>
+          <select
+            label="type"
+            value={input.type}
+            onChange={handleChange}
+            name="type"
+          >
+            <option>Paper craft</option>
+            <option>Knitting</option>
+            <option>Sewing</option>
+            <option>Painting and drawing</option>
+            <option>Collage</option>
+            <option>Fimo and clay</option>
+            {/* Option to write own type */}
+          </select>
+        </div>
+        <div>
+          <h3>3. What are your materials?</h3>
+          <input
+            type="text"
+            label="materials"
+            name="materials"
+            value={input.materials}
+            onChange={handleChange}
+          ></input>
+        </div>
+        <div>
+          <h3>4. Describe your project</h3>
+          <input
+            type="text"
+            label="description"
+            name="description"
+            value={input.description}
+            onChange={handleChange}
+          ></input>
+        </div>
+        {/* Is it possible to map through preset images and insert as component? */}
+        <div>
+          <h3>5. Add an image</h3>
+          <p>
+            You can also add a photo or your project later when you have
+            finished.
+          </p>
+          <input
+            type="text"
+            label="image url"
+            placeholder="image url"
+            name="image"
+            value={input.image}
+            onChange={handleChange}
+          ></input>
+          <h3>or choose one</h3>
+          {/* //Make sure no buttons are selected on load */}
+          {/* <label>
+                <input type="radio" id="image1" name="presetImage" />
+                <img
+                  className="image-select"
+                  src="src/assets/knitting.jpg"
+                  alt="knitting"
+                />
+              </label>
+    
+              <label>
+                <input
+                  type="radio"
+                  id="image2"
+                  name="presetImage"
+                  // value={src/assets/scrapbook.jpg}
+                />
+                <img
+                  className="image-select"
+                  src="src/assets/scrapbook.jpg"
+                  alt="Scrapbooking"
+                />
+              </label> */}
+        </div>
+        <button>Create!</button>
+      </form>
+    </>
+  );
+}
+
+export default Create;
